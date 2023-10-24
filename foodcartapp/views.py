8c24dcf27c4890dde73cf0d618de43e2a423e822
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-24 18:40:03 +0400

    поигрался с функцией register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.templatetags.static import static
-
+import json
 
 from .models import Product
 
@@ -58,5 +58,7 @@ def product_list_api(request):
 
 
 def register_order(request):
-    # TODO это лишь заглушка
+
+    data = json.loads(request.body.decode())
+    print(data)
     return JsonResponse({})
