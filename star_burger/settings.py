0089b6dd8f54c3cedf487bf40ecd8558c7dfc19e
Valeriy Lvov <tepliy_shov@mail.ru>
2023-11-09 10:53:24 +0400

    changed settings

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -118,6 +118,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+if DEBUG:
+    STATICFILES_DIRS = (
+        BASE_DIR / 'static',
+    )
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
